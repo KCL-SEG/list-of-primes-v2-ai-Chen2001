4911d691af1546c40f7db0c6278ce09120f92211
ACynthiaa <k20101949@kcl.ac.uk>
2022-09-27 20:31:49 +0800

    new version

--- primes.py
+++ primes.py
@@ -1,6 +1,16 @@
-"""List of prime numbers generator."""
-"""ENTER YOUR SOLUTION HERE!"""
-
 def primes(number_of_primes):
+    if number_of_primes<=0:
+        raise ValueError(f"the number of primes must be positive")
     list = []
+    number=2
+    while len(list)<number_of_primes:
+        n=2
+        while n*n<number:
+            if number%n!=0:
+                n+=1
+            else:
+                break
+        if n*n>number:
+            list.append(number)
+        number+=1       
     return list
